3df7e50d70cf6c02e66708986b8cdfaa0e03f7c3
Hemant Ashta <hemant@workongrid.com>
2023-11-14 07:44:07 +0530

    user and peer screen share option added

--- src/context/RoomContext.tsx
+++ src/context/RoomContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useReducer, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import socketIOClient from "socket.io-client";
-import Peer from "peerjs";
+import Peer, { PeerConnectOption } from "peerjs";
 import { v4 as uuidV4 } from "uuid";
 import { peersReducer } from "./peerReducer";
 import { addPeerAction, removePeerAction } from "./peerActions";
@@ -20,6 +20,10 @@ export const RoomProvider: React.FunctionComponent<Props> = ({children}) => {
     const [user, setUser] = useState<Peer>()
     const [stream, setStream] = useState<MediaStream>()
     const [peers, dispatch] = useReducer(peersReducer, {})
+    const [shareScreenId, setShareScreenId] = useState<string>("")
+    const [isUserSharingScreen, setIsUserSharingScreen] = useState<boolean>(false)
+    const [isPeerSharingScreen, setIsPeerSharingScreen] = useState<boolean>(false)
+    const [roomId, setRoomId] = useState<string>("")
 
     const enterRoom = ({ roomId }: { roomId: "string" }) => {
         navigate(`/room/${roomId}`)
@@ -33,9 +37,39 @@ export const RoomProvider: React.FunctionComponent<Props> = ({children}) => {
         dispatch(removePeerAction(peerId))
     }
 
+    const switchScreen = (stream: MediaStream) => {
+        setStream(stream)
+        setShareScreenId(user?.id || "")
+        setIsUserSharingScreen(!isUserSharingScreen)
+
+        //for sharing your stream (Screen || Video) with other users
+        Object.values(user?.connections).forEach((connection: any) => {
+            const videoTrack = stream?.getTracks().find((track) => track.kind === 'video')
+            connection[0]
+                .peerConnection
+                .getSenders()[1]
+                .replaceTrack(videoTrack)
+                .catch((err: any) => console.error(err))
+        })
+    }
+
+    const shareScreen = () => {
+        if(isSharingScreen){
+            navigator.mediaDevices.getUserMedia({ video: true, audio: true}).then(switchScreen)
+        } else {
+            navigator.mediaDevices.getDisplayMedia({}).then(switchScreen)
+        }
+    }
+
     useEffect(() => {
         const userId = uuidV4()
         const peer = new Peer(userId)
+        //FOR PEER JS CLOUD DOWN BUG, CONFIG FOR CREATE YOUR OWN LOCAL PEER SERVER
+        // {
+        //     host: 'localhost',
+        //     port: 9001,
+        //     path: "/",
+        // }
         setUser(peer)
         try{
             navigator.mediaDevices
@@ -49,21 +83,55 @@ export const RoomProvider: React.FunctionComponent<Props> = ({children}) => {
         ws.on('room-created', enterRoom)
         ws.on('get-users-list', getUsersList)
         ws.on('user-disconnected', removePeer)
+        ws.on('user-started-sharing', (peerId) => {
+            console.log('started')
+            setIsPeerSharingScreen(true)
+            setShareScreenId(peerId)
+        })
+        ws.on('user-stopped-sharing', () => { 
+            console.log('stopped')
+            setIsPeerSharingScreen(false)
+            setShareScreenId("")
+        })
+
+        return () => {
+            ws.off("room-created");
+            ws.off("get-users-list");
+            ws.off("user-disconnected");
+            ws.off("user-shared-screen");
+            ws.off("user-stopped-sharing");
+            ws.off("user-joined");
+        };
     }, []) 
+
+    useEffect(() => {
+        if(!isPeerSharingScreen){
+            if(isUserSharingScreen && shareScreenId){
+                console.log('start')
+                ws.emit('start-sharing', {
+                    peerId: shareScreenId,
+                    roomId,
+                })
+            } else {
+                console.log('stop')
+                ws.emit('stop-sharing')
+            }
+        } else {
+            //your peer is already sharing the screen
+        }
+    }, [isUserSharingScreen, isPeerSharingScreen, shareScreenId, roomId])
 
     useEffect(() => {
         if(!user || !stream) return;
 
         ws.on('user-joined', ({ peerId } : { peerId: string; roomId: string; }) => {
             const call = user.call(peerId, stream)
-            console.log('user-joined', peerId, call)
             call.on("stream", (peerStream: MediaStream) => {
                 dispatch(addPeerAction(peerId,  peerStream))
             })
         })
 
         user.on("call", (call) => {
-            console.log('call-user-joined', call)
             call.answer(stream)
             call.on("stream", (peerStream) => {
                 dispatch(addPeerAction(call.peer,  peerStream))
@@ -71,10 +139,10 @@ export const RoomProvider: React.FunctionComponent<Props> = ({children}) => {
         })
     }, [user, stream])
 
-    console.log(peers)
+    const isSharingScreen = isUserSharingScreen || isPeerSharingScreen
 
     return(
-        <RoomContext.Provider value={{ ws, user, stream, peers }}>
+        <RoomContext.Provider value={{ ws, user, stream, peers, shareScreenId, isSharingScreen, shareScreen, setRoomId }}>
             {children}
         </RoomContext.Provider>
     )
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,10 +2,11 @@ import { useContext, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { RoomContext } from "../context/RoomContext"
 import { VideoPlayer } from "../components/VideoPlayer"
+import { ShareScreenButton } from "../components/ShareScreenButton"
 
 export const Room = () => {
     const { roomId } = useParams()
-    const { ws, user, stream, peers } = useContext(RoomContext)
+    const { ws, user, stream, peers, shareScreen, shareScreenId, isSharingScreen, setRoomId } = useContext(RoomContext)
 
     useEffect(() => {
         user?.on("open", () => {
@@ -13,16 +14,36 @@ export const Room = () => {
         });
     }, [roomId, user, ws])
 
+    useEffect(() => {
+        setRoomId(roomId)
+    }, [roomId, setRoomId])
+
+    const screenShareVideo = shareScreenId === user?.id ? stream : peers[shareScreenId]?.stream
+    const { [shareScreenId]: sharing, ...peersToShow} = peers
+
+    console.log(screenShareVideo)
+
     return(
         <>
             <div>Room Id: {roomId}</div>
-            <div className="grid grid-cols-4 gap-4">
-                <VideoPlayer className="me" key={"me"} stream={stream} />
+            <div className="flex">
                 {
-                    Object.values(peers).map((peer: any) => {
-                        return(<VideoPlayer key={peer.id} stream={peer.stream}/>)
-                    })
+                    screenShareVideo &&
+                    <div className="w-4/5 pr-4">
+                        <VideoPlayer stream={screenShareVideo} />
+                    </div>
                 }
+                <div className={`grid gap-4 ${isSharingScreen ?  "w-1/5 grid-cols-1" : "grid-cols-4"}`}>
+                    {shareScreenId !== user?.id && <VideoPlayer className="me" key={"me"} stream={stream} />}
+                    {
+                        Object.values(peersToShow).map((peer: any) => {
+                            return(<VideoPlayer key={peer.id} stream={peer.stream}/>)
+                        })
+                    }
+                </div>
+            </div>
+            <div className="fixed bottom-0 p-6 w-full flex justify-center border-t-2 bg-white">
+                <ShareScreenButton onClick={shareScreen}/>
             </div>
         </>
     )
